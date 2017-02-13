c5e5e1d57f1d88a3191ac3027137c7daffed780f
ahljane <ahljanebelogolo@bsit.usjr.edu.ph>
2017-02-13 17:08:45 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/pcpc/albumsearchmidtermexam/AlbumLoader.java
@@ -0,0 +1,35 @@
+package com.example.pcpc.albumsearchmidtermexam;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pcpc on 11/02/2017.
+ */
+
+public class AlbumLoader extends AsyncTaskLoader<ArrayList<Album>>{
+
+    private String mUrl;
+    private int requestCode;
+
+    public AlbumLoader(Context context, String mUrl, int requestCode) {
+        super(context);
+        this.mUrl = mUrl;
+        this.requestCode = requestCode;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public ArrayList<Album> loadInBackground() {
+        if(mUrl == null)
+            return null;
+
+        return QueryUtils.fetchAlbumData(mUrl,requestCode);
+    }
+}
